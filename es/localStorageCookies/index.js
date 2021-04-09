1ea4a4c4090a88d42bd3ce52297ff640effb491c
Mai Xuan Hien <hienmx@fpt.com.vn>
2021-04-09 14:37:53 +0700

    update f1-token

--- es/localStorageCookies/index.js
+++ es/localStorageCookies/index.js
@@ -1,16 +1,16 @@
 import Cookies from 'js-cookie';
-var JWT_TOKEN = process.env.JWT_TOKEN || 'fwork-token';
-export function setToken(token) {
-  Cookies.set(JWT_TOKEN, token);
+var JWT_TOKEN = process.env.JWT_TOKEN || process.env.REACT_APP_JWT_TOKEN;
+export function setToken ( token ) {
+  Cookies.set( JWT_TOKEN, token );
 }
-export function setTokenExpire(token, day) {
-  Cookies.set(JWT_TOKEN, token, {
+export function setTokenExpire ( token, day ) {
+  Cookies.set( JWT_TOKEN, token, {
     expires: day
-  });
+  } );
 }
-export function getToken() {
-  return Cookies.get(JWT_TOKEN);
+export function getToken () {
+  return Cookies.get( JWT_TOKEN );
 }
-export function removeToken() {
-  Cookies.remove(JWT_TOKEN);
+export function removeToken () {
+  Cookies.remove( JWT_TOKEN );
 }
--- src/localStorageCookies/index.js
+++ src/localStorageCookies/index.js
@@ -1,18 +1,18 @@
 import Cookies from 'js-cookie';
-const JWT_TOKEN = process.env.JWT_TOKEN || 'fwork-token';
+const JWT_TOKEN = process.env.JWT_TOKEN || process.env.REACT_APP_JWT_TOKEN;
 
-export function setToken(token) {
-  Cookies.set(JWT_TOKEN, token);
+export function setToken ( token ) {
+  Cookies.set( JWT_TOKEN, token );
 }
 
-export function setTokenExpire(token, day) {
-  Cookies.set(JWT_TOKEN, token, { expires: day });
+export function setTokenExpire ( token, day ) {
+  Cookies.set( JWT_TOKEN, token, { expires: day } );
 }
 
-export function getToken() {
-  return Cookies.get(JWT_TOKEN);
+export function getToken () {
+  return Cookies.get( JWT_TOKEN );
 }
 
-export function removeToken() {
-  Cookies.remove(JWT_TOKEN);
+export function removeToken () {
+  Cookies.remove( JWT_TOKEN );
 }
